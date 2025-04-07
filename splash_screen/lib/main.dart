525cab450d0fc152170e38d0afbaf61e11f2d643
DaeYoung Jeong <eodud1025@naver.com>
2025-04-07 17:07:19 +0900

    feat: stateLessWidget 사용, 단축키 학습

--- splash_screen/lib/main.dart
+++ splash_screen/lib/main.dart
@@ -3,20 +3,33 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xFF6390ff),
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 28.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('asset/img/logo.png'),
-              SizedBox(height: 20.0),
-              CircularProgressIndicator(color: Colors.white),
-            ],
-          ),
-        ),
-      ),
+      home: HomeScreen()
     ),
   );
 }
+
+// stless 단축키 활용
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF6390ff),
+      // option + enter 단축키 사용
+      // wrap this widget, remove this widget 사용해보기
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 28.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('asset/img/logo.png'),
+            SizedBox(height: 20.0),
+            CircularProgressIndicator(color: Colors.white),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
