5689e0aaad6d2acd401b59b25673f373b3d96bed
DaeYoung Jeong <eodud1025@naver.com>
2025-04-07 16:42:06 +0900

    chore: 프로젝트 생성

---
+++ splash_screen/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.black,
+            body: Center(
+              child: Text(
+                'code Factory',
+                style: TextStyle(color: Colors.white)
+              ),
+            )
+        ),
+      )
+  );
+}
