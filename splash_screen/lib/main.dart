5c2fd83d40ecd6f73a2f9385973994e4ea088f14
DaeYoung Jeong <eodud1025@naver.com>
2025-04-07 17:03:07 +0900

    feat: sized 박스으로 마진 설정

--- splash_screen/lib/main.dart
+++ splash_screen/lib/main.dart
@@ -2,24 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-      MaterialApp(
-        home: Scaffold(
-            backgroundColor: Color(0xFF6390ff),
-            body: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 28.0
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('asset/img/logo.png'),
-                  CircularProgressIndicator(
-                    color: Colors.white,
-                  )
-                ],
-              ),
-            )
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFF6390ff),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 28.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('asset/img/logo.png'),
+              SizedBox(height: 20.0),
+              CircularProgressIndicator(color: Colors.white),
+            ],
+          ),
         ),
-      )
+      ),
+    ),
   );
 }
