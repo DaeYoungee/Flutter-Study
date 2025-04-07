c3b63aee9e059d3e9ed9d3c4ac270361fe8b027f
DaeYoung Jeong <eodud1025@naver.com>
2025-04-07 17:01:44 +0900

    feat: 패딩 설정

--- splash_screen/lib/main.dart
+++ splash_screen/lib/main.dart
@@ -5,11 +5,19 @@ void main() {
       MaterialApp(
         home: Scaffold(
             backgroundColor: Color(0xFF6390ff),
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('asset/img/logo.png')
-              ],
+            body: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 28.0
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('asset/img/logo.png'),
+                  CircularProgressIndicator(
+                    color: Colors.white,
+                  )
+                ],
+              ),
             )
         ),
       )
