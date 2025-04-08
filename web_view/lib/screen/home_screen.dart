f2b884009a9c4899429d76f180f472e206f77655
DaeYoung Jeong <eodud1025@naver.com>
2025-04-08 16:45:38 +0900

    feat: webview JavaScriptMode 설정

--- web_view/lib/screen/home_screen.dart
+++ web_view/lib/screen/home_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-final uri = Uri.parse('https://blog.codefactory.ai');
+// final uri = Uri.parse('https://blog.codefactory.ai');
+final uri = Uri.parse('https://youtube.com');
+
 
 class HomeScreen extends StatelessWidget {
-  WebViewController controller = WebViewController()..loadRequest(uri);
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(uri);
+
   HomeScreen({super.key});
 
   @override
@@ -14,6 +19,14 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.orange,
         title: const Text('Home'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              controller.loadRequest(uri);
+            },
+            icon: Icon(Icons.home),
+          ),
+        ],
       ),
       body: WebViewWidget(controller: controller),
     );
