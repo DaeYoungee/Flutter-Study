1b5264ba831824b2a7b5f67ea879a67558bb1924
DaeYoung Jeong <eodud1025@naver.com>
2025-04-16 16:11:33 +0900

    feat: timer 적용(자동 스크롤 뷰 구현)

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Timer? timer;
+  PageController pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    timer = Timer.periodic(Duration(seconds: 2), (Timer timer) {
+      int currentPage = pageController.page!.toInt();
+      int nextPage = (++currentPage) % 5;
+      pageController.animateToPage(
+        nextPage,
+        duration: Duration(microseconds: 500),
+        curve: Curves.linear,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    if (timer != null) {
+      timer!.cancel();
+    }
+    pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
-        children:
-            [
-              1,
-              2,
-              3,
-              4,
-              5,
-            ].map((e) => Image.asset('asset/img/image_$e.jpeg')).toList(),
+        controller: pageController,
+        children: [1, 2, 3, 4, 5,]
+            .map(
+                (e) => Image.asset('asset/img/image_$e.jpeg')
+            ).toList(),
       ),
     );
   }
