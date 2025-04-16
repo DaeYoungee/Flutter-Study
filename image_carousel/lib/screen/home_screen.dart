908d8f25f24b938c9ca1655da4447d1534934a78
DaeYoung Jeong <eodud1025@naver.com>
2025-04-16 15:59:32 +0900

    feat: pageview 적용

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 
-
-
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          child: Image.asset('asset/img/image_1.jpeg'),
-        ),
+      body: PageView(
+        children:
+            [
+              1,
+              2,
+              3,
+              4,
+              5,
+            ].map((e) => Image.asset('asset/img/image_$e.jpeg')).toList(),
       ),
     );
   }
