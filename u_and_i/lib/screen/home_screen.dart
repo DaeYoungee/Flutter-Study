e4a7f72695c941196d79ee3be355f992abe66136
DaeYoung Jeong <eodud1025@naver.com>
2025-04-17 00:42:53 +0900

    feat: DatePicker 구현, StatefulWidget 변경, 날짜 리소스 상태로 변경

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -25,11 +26,18 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class _Top extends StatelessWidget {
+class _Top extends StatefulWidget {
   const _Top({super.key});
 
   @override
+  State<_Top> createState() => _TopState();
+}
+
+class _TopState extends State<_Top> {
+  DateTime selectDated = DateTime.now();
+  @override
   Widget build(BuildContext context) {
+    final now = DateTime.now();
     final textTheme = Theme.of(context).textTheme;
 
     return Expanded(
@@ -37,14 +45,37 @@ class _Top extends StatelessWidget {
         children: [
           Text("U&I", style: textTheme.displayLarge),
           Text("우리 처음 만날 날", style: textTheme.bodyLarge),
-          Text("2023-11-23", style: textTheme.bodyMedium),
+          Text("${selectDated.year}-${selectDated.month}-${selectDated.day}", style: textTheme.bodyMedium),
 
           IconButton(
             iconSize: 60.0,
-            onPressed: () {},
+            onPressed: () {
+              showCupertinoDialog(
+                context: context,
+                barrierDismissible: true,
+                builder: (BuildContext context) {
+                  return Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      height: 300.0,
+                      color: Colors.white,
+                      child: CupertinoDatePicker(
+                        mode: CupertinoDatePickerMode.date,
+                        dateOrder: DatePickerDateOrder.ymd,
+                        onDateTimeChanged: (DateTime newDateTime) {
+                          setState(() {
+                            selectDated = newDateTime;
+                          });
+                        },
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
             icon: Icon(Icons.favorite, color: Colors.red),
           ),
-          Text("D+1", style: textTheme.displayMedium),
+          Text("D+${now.difference(selectDated).inDays+1}", style: textTheme.displayMedium),
         ],
       ),
     );
